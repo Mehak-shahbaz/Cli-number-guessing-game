072f680dc3cd734a95262ff97ca33465dbe0fb81
Mehak-shahbaz <164498970+Mehak-shahbaz@users.noreply.github.com>
2024-04-21 01:58:22 +0500

    Add files via upload
    Make a amaizing number guessing game

---
+++ index.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+import { log } from "console";
+import inquirer from "inquirer";
+
+console.log("\n\tWellcome to code with Mehak-CLI Number Guessing Game\n");
+
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Enter your guess number(Number limit from 1 to 5):",
+    }
+]);
+if(answer.userGuessedNumber === randomNumber){
+    console.log("Congratulation ! You guess a correct number.");
+}
+else{
+    console.log("You guess a wrong number");
+}    
